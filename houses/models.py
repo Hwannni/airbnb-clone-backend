65eeb8a29e6061cfb3559270bbd98b52916a8ce1
Hwannni <cmhcms1115@naver.com>
2024-08-13 16:29:39 +0900

    24_08_13_airbnb_clone

--- houses/models.py
+++ houses/models.py
@@ -6,6 +6,10 @@ class House(models.Model):
     """Model Definition for House"""
 
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
-    description = models.TimeField()
+    price_per_night = models.PositiveIntegerField()
+    description = models.TextField()
     address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
