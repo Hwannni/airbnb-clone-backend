2f638bb8f9c8c04d17aab1ef754ad09a681d9f29
Hwannni <cmhcms1115@naver.com>
2024-08-26 21:39:39 +0900

    15_3_Token_Authentication

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 
@@ -8,6 +9,7 @@ urlpatterns = [
     path("change-password", views.ChangePassword.as_view()),
     path("log-in", views.LogIn.as_view()),
     path("log-out", views.LogOut.as_view()),
+    path("token-login", obtain_auth_token),
     # @는 인스타에서도 사용하니까 그냥 붙이는거.
     path("@<str:username>", views.PublicUser.as_view()),
     path("@<str:username>/rooms", views.UserRooms.as_view()),
