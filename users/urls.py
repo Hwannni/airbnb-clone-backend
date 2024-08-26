b7ad7161ed64ff252b0f190e02e075022f3ff380
Hwannni <cmhcms1115@naver.com>
2024-08-27 00:35:40 +0900

    15_4_JWT_Econde

--- users/urls.py
+++ users/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("log-in", views.LogIn.as_view()),
     path("log-out", views.LogOut.as_view()),
     path("token-login", obtain_auth_token),
+    path("jwt-login", views.JWTLogIn.as_view()),
     # @는 인스타에서도 사용하니까 그냥 붙이는거.
     path("@<str:username>", views.PublicUser.as_view()),
     path("@<str:username>/rooms", views.UserRooms.as_view()),
