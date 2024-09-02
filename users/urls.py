37422b2273db2c560dc6f8e2e4b7d6e1cdf8d7c9
Hwannni <cmhcms1115@naver.com>
2024-09-02 23:47:48 +0900

    GithubURL_Views

--- users/urls.py
+++ users/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
     path("change-password", views.ChangePassword.as_view()),
-    path("log-in", views.LogIn.as_view()), # 직접 구현한 로그인
-    path("log-out", views.LogOut.as_view()), # 직접 구현한 로그아웃
-    path("token-login", obtain_auth_token), # Django Rest Framework의 토큰 인증
-    path("jwt-login", views.JWTLogIn.as_view()), # JWT를 이용한 토큰 인증
+    path("log-in", views.LogIn.as_view()),  # 직접 구현한 로그인
+    path("log-out", views.LogOut.as_view()),  # 직접 구현한 로그아웃
+    path("token-login", obtain_auth_token),  # Django Rest Framework의 토큰 인증
+    path("jwt-login", views.JWTLogIn.as_view()),  # JWT를 이용한 토큰 인증
+    path("github", views.GithubLogIn.as_view()),
     # @는 인스타에서도 사용하니까 그냥 붙이는거.
     path("@<str:username>", views.PublicUser.as_view()),
     path("@<str:username>/rooms", views.UserRooms.as_view()),
